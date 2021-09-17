3bde55058359d631c0a4345a4809482e13f9d04e
Ignacio Buioli <ibuioli@gmail.com>
2021-09-17 15:38:55 -0300

    feat: format report

--- l10n_ar_report_fe/__manifest__.py
+++ l10n_ar_report_fe/__manifest__.py
@@ -5,11 +5,10 @@
     'author': "Moldeo Interactive, Exemax, Codize, ADHOC SA, Odoo Community Association (OCA)",
     'category': 'Location/Argentina',
     'depends': [
-        'l10n_ar_afipws_fe',
+        'l10n_ar_afipws_fe'
     ],
     'installable': True,
     'license': 'AGPL-3',
-    'name': 'l10n_ar_report_fe',
     'data': [
         'report_invoice_fe.xml',
     ],
