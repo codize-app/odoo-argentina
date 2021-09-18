8d623afc32e3e3e7bce59ae012a24e8cea5c4225
Ignacio Buioli <ibuioli@gmail.com>
2021-09-18 18:12:44 -0300

    feat: add account_financial_amount

---
+++ account_financial_amount/README.md
@@ -0,0 +1,16 @@
+# Accounting Financial Amounts
+
+Add financial amounts on account move lines.
+
+IMPORTANT: This module should not be installed alone, it is going to be installed by other modules automatically.
+
+## Contributors
+
+* AdHoc SA
+* Moldeo Interactive
+* Exemax
+* Codize
+
+## Maintainer
+
+This module is maintained by the Exemax-Codize. Original develop by AdHoc SA
---
+++ account_financial_amount/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Accounting Financial Amounts",
+    "version": "14.0.1.0.0",
+    "author": "ADHOC SA, Exemax, Codize",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        'security/account_debt_management_security.xml',
+        'wizard/res_config_settings_views.xml',
+    ],
+    'installable': True,
+}
