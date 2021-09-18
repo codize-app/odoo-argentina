de0b0c037cebaefa9b991b996c9e5eb166f651a4
Ignacio Buioli <ibuioli@gmail.com>
2021-09-18 18:06:24 -0300

    feat: add account_payment_fix

---
+++ account_payment_fix/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Account Payment Fix',
+    'version': '14.0.1.0.0',
+    'category': 'Account',
+    'author': 'ADHOC SA, Exemax, Codize',
+    'license': 'AGPL-3',
+    'data': [
+        'views/account_payment_view.xml',
+    ],
+    'demo': [],
+    'depends': [
+        'account',
+    ],
+    'installable': True
+}
