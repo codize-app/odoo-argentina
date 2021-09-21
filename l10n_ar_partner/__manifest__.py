95e2234a16324aa0329eaa5cffecc5e3a23b1f8e
Ignacio Buioli <ibuioli@gmail.com>
2021-09-21 11:25:57 -0300

    feat: initial partner commit

---
+++ l10n_ar_partner/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Datos Extras para Contacto de Argentina',
+    'version': '14.0.0.0.0',
+    'category': 'Partner',
+    'license': 'AGPL-3',
+    'summary': "Datos Extras para Contacto de Argentina",
+    'description': """
+Datos Extras para Contacto de Argentina
+======================
+
+* Agrega nombre de Fantasía
+    """,
+    'author': 'Codize, Exemax',
+    'website': 'http://www.codize.ar',
+    'depends': ['base'],
+    'data': ['partner_view.xml'],
+    'installable': True,
+}
