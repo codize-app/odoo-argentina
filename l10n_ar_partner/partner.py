3ee9fd922525e208c6ad9e0e6b6ad52dff203d77
Ignacio Buioli <ibuioli@gmail.com>
2021-09-21 11:50:02 -0300

    feat: partner reference & payment view

--- l10n_ar_partner/partner.py
+++ l10n_ar_partner/partner.py
@@ -6,3 +6,12 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     internal_reference = fields.Char('Nombre de Fantasía')
+
+    def name_get(self):
+        result = []
+        for record in self:
+            if record.internal_reference:
+                result.append((record.id, record.internal_reference + ' - [' + record.name + ']'))
+            else:
+                result.append((record.id, record.name))
+        return result
