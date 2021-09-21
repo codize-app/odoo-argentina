629efbb972bfed2027ffe1a283c74639a76afa8f
Ignacio Buioli <ibuioli@gmail.com>
2021-09-21 20:55:50 -0300

    feat: add internal taxes

--- l10n_ar_taxes/partner.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class ResPartner(models.Model):
-    _inherit = 'product.template'
-
-    has_internal_taxes = fields.Boolean('¿Tiene Impuestos Internos?')
-    internal_taxes = fields.Float('Impuesto Interno', default=0)
