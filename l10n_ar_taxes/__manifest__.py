9692b1f5be025f0f62798cbf662f126d455c0d26
Ignacio Buioli <ibuioli@gmail.com>
2021-09-22 13:48:07 -0300

    feat: account_check without payment_group & internal_taxes on move

--- l10n_ar_taxes/__manifest__.py
+++ l10n_ar_taxes/__manifest__.py
@@ -13,6 +13,6 @@ Impuestos Extras Argentina
     'author': 'Codize, Exemax',
     'website': 'http://www.codize.ar',
     'depends': ['base', 'product', 'account', 'sale'],
-    'data': ['product_template.xml', 'sale_order.xml'],
+    'data': ['product_template.xml', 'sale_order.xml', 'account_move.xml'],
     'installable': True,
 }
