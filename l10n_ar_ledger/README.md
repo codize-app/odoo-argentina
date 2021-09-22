aa259b837ed97995b8404b9eaa265f72ae38bffa
Ignacio Buioli <ibuioli@gmail.com>
2021-09-22 14:20:56 -0300

    feat: add ledger

---
+++ l10n_ar_ledger/README.md
@@ -0,0 +1,14 @@
+# Argentina Ledger
+
+Creates Sale and Purchase VAT report menus in "Accounting/Reporting/Argentina Statements"
+
+## Contributors
+
+* AdHoc SA
+* Moldeo Interactive
+* Exemax
+* Codize
+
+## Maintainer
+
+This module is maintained by the Exemax-Codize. Original develop by AdHoc SA
---
+++ l10n_ar_ledger/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Libros IVA de Argentina",
+    'version': '14.0.0.0.0',
+    'category': 'Accounting/Localizations',
+    'sequence': 14,
+    'author': 'ADHOC SA, Moldeo Interactive, Odoo Community Association (OCA), Exemax, Codize',
+    'license': 'AGPL-3',
+    'summary': '',
+    "depends": [
+	    "base",
+        "l10n_ar",
+        "report_xlsx"
+    ],
+    'external_dependencies': {
+    },
+    "data": [
+        'account_vat_report_view.xml',
+        'account_report.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
