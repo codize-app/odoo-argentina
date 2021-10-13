b425fc071d683928b902e8100559749ebb4f81b2
Ignacio Buioli <ibuioli@gmail.com>
2021-10-13 11:47:03 -0300

    feat: add report picking - electronic remito

---
+++ l10n_ar_report_picking/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, RedirectWarning
+import logging
+import datetime
+from datetime import datetime, timedelta, date
+import json
+import base64
+
+class AccountMoveLine(models.Model):
+    _inherit = 'stock.picking'
+
+    afip_cai = fields.Date('CAI')
+    afip_cai_due = fields.Date('Fecha de Vencimiento CAI')
