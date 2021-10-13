7327ccabbed95d1d3adf17d9c810d14a6e111527
Ignacio Buioli <ibuioli@gmail.com>
2021-10-13 11:47:03 -0300

    feat: add report picking - electronic remito

---
+++ l10n_ar_report_picking/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Reporte del Remito Argentino",
+    'version': '14.0',
+    'author': "Exemax, Codize",
+    'category': 'Accounting/Localizations',
+    'depends': [
+        'stock',
+        'l10n_ar'
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+    'data': [
+        'report_stock_picking.xml',
+        'afip_view.xml'
+    ],
+    'demo': []
+}
