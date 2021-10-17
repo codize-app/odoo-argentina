16f84c6dd9c2ff758381201f6e1b2e02a0d3cb90
Ignacio Buioli <ibuioli@gmail.com>
2021-10-17 15:21:46 -0300

    feat: add states in partner

--- l10n_ar_partner/partner.py
+++ l10n_ar_partner/partner.py
@@ -25,8 +25,8 @@ class ResPartner(models.Model):
         connect = ws_sr_padron_a5.Consultar(self.vat)
         #_logger.info(ws_sr_padron_a5)
         self.name = ws_sr_padron_a5.denominacion
-        self.street = ws_sr_padron_a5.direccion
-        self.city = ws_sr_padron_a5.localidad
+        self.street = ws_sr_padron_a5.direccion.capitalize()
+        self.city = ws_sr_padron_a5.localidad.capitalize()
         self.zip = ws_sr_padron_a5.cod_postal
         if ws_sr_padron_a5.tipo_persona == 'FISICA':
             self.company_type = 'person'
@@ -48,5 +48,24 @@ class ResPartner(models.Model):
             l10n_ar_type = 'Consumidor Final'
 
         if l10n_ar_type != '':
-            iva_afip = self.env['l10n_ar.afip.responsibility.type'].search([('name', '=', l10n_ar_type)])
+            iva_afip = self.env['l10n_ar.afip.responsibility.type'].search([('name', '=', l10n_ar_type)], limit=1)
             self.l10n_ar_afip_responsibility_type_id = iva_afip.id
+
+        if ws_sr_padron_a5.provincia:
+            country_id = self.env['res.country'].search([('name', '=', 'Argentina')], limit=1)
+            if country_id:
+                self.country_id = country_id.id
+
+            provincia = ''
+
+            if ws_sr_padron_a5.provincia == 'NEUQUEN':
+                provincia = 'Neuquén'
+            elif ws_sr_padron_a5.provincia == 'CORDOBA':
+                provincia = 'Córdoba'
+            else:
+                provincia = ws_sr_padron_a5.provincia
+
+            state_id = self.env['res.country.state'].search([('name', 'like', provincia), ('country_id', '=', country_id.id)], limit=1)
+            if state_id:
+                self.state_id = state_id.id
+
