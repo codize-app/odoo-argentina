e6f3774d25e60771a8e976c39447a9f7ed51298f
root <admin@autodidactati.com>
2022-07-18 20:15:30 -0300

    [UPD] move per/ret IIBB

--- l10n_ar_withholding_arba/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import account_export_arba
-from . import padron_arba
-from . import import_padron_arba
-from . import res_partner_inherit
-from . import account_payment_group_inherit
-from . import account_move_inherit
-from . import res_config_settings_inherit
-from . import account_tax_inherit
