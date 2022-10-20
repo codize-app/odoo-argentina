2f853beb5c2ea63990425c490374c5a441da778a
Ignacio Buioli <ibuioli@gmail.com>
2022-10-20 08:50:45 -0300

    fix: internal transfer

--- account_payment_group/models/account_journal_dashboard.py
+++ account_payment_group/models/account_journal_dashboard.py
@@ -1,21 +1,32 @@
-from odoo import models, api
+from odoo import models, api, _
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    def open_payments_action(self, payment_type, mode='form'):
+    def open_payments_action(self, payment_type, mode='tree'):
         if payment_type == 'transfer':
             ctx = self._context.copy()
-            ctx.update({
-                'default_payment_type': payment_type,
-                'default_journal_id': self.id
-            })
-            ctx.pop('group_by', None)
-            action_rec = self.env['ir.model.data'].xmlid_to_object(
-                'account_payment_group.action_account_payments_transfer')
-            action = action_rec.read([])[0]
-            action['context'] = ctx
-            action['domain'] = [('journal_id', '=', self.id),
-                                ('payment_type', '=', payment_type)]
+            if mode == 'form':
+                ctx.update({
+                    'default_payment_type': 'transfer',
+                    'default_journal_id': self.id,
+                    'default_partner_type': 'customer',
+                    'default_is_internal_transfer': True,
+                    'is_internal_transfer': True,
+                    'default_partner_id': self.company_id.partner_id.id,
+                })
+                action_rec = self.env.ref(
+                    'account_payment_group.action_account_payments_transfer_form')
+                action = action_rec.read([])[0]
+                action['name'] = _('New Transfer')
+                action['context'] = ctx
+            else:
+                action_rec = self.env.ref(
+                    'account_payment_group.action_account_payments_transfer')
+
+                action = action_rec.read([])[0]
+                action['context'] = ctx
+                action['domain'] = [('journal_id', '=', self.id),
+                                    ('payment_type', '=', payment_type)]
             return action
         return super(AccountJournal, self).open_payments_action(payment_type)
--- account_payment_group/models/account_payment.py
+++ account_payment_group/models/account_payment.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
+
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -260,13 +261,16 @@ class AccountPayment(models.Model):
         we disable change of partner_type if we came from a payment_group
         but we still reset the journal
         """
-        if not self._context.get('payment_group'):
-            return super(AccountPayment, self)._onchange_payment_type()
-        self.journal_id = False
+        # if not self._context.get('payment_group'):
+        #     return super(AccountPayment, self)._onchange_payment_type()
+        # self.journal_id = False
+        pass
 
-    @api.constrains('payment_group_id', 'payment_type')
+    @api.constrains('payment_group_id', 'payment_type', 'is_internal_transfer')
     def check_payment_group(self):
         # odoo tests don't create payments with payment gorups
+        if self.env.company.country_id.code != 'AR':
+            return True
         if self.env.registry.in_test_mode():
             return True
         for rec in self:
@@ -275,12 +279,12 @@ class AccountPayment(models.Model):
                     'receivable', 'payable']
                 for x in self._context.get('counterpart_aml_dicts', [])])
             if rec.partner_type and rec.partner_id and receivable_payable and \
-               not rec.payment_group_id:
+               not rec.payment_group_id and not rec.is_internal_transfer:
                 raise ValidationError(_(
                     'Payments with partners must be created from '
                     'payments groups'))
             # transfers or payments from bank reconciliation without partners
-            elif not rec.partner_type and rec.payment_group_id:
+            elif not rec.partner_type and rec.payment_group_id and not rec.is_internal_transfer:
                 raise ValidationError(_(
                     "Payments without partners (usually transfers) cant't "
                     "have a related payment group"))
@@ -463,3 +467,114 @@ class AccountPayment(models.Model):
                     #raise ValidationError('estamos aca %s %s'%(amount_credit,res[0]['line_ids'][0][2]))
                     res[i]['line_ids'][1][2]['debit'] = rec.signed_amount_company_currency
         return res
+
+    def _prepare_move_line_default_vals(self, write_off_line_vals=None):
+        ''' Prepare the dictionary to create the default account.move.lines for the current payment.
+        :param write_off_line_vals: Optional dictionary to create a write-off account.move.line easily containing:
+            * amount:       The amount to be added to the counterpart amount.
+            * name:         The label to set on the line.
+            * account_id:   The account on which create the write-off.
+        :return: A list of python dictionary to be passed to the account.move.line's 'create' method.
+        '''
+        self.ensure_one()
+        write_off_line_vals = write_off_line_vals or {}
+
+        if not self.journal_id.payment_debit_account_id or not self.journal_id.payment_credit_account_id:
+            raise UserError(_(
+                "You can't create a new payment without an outstanding payments/receipts account set on the %s journal.",
+                self.journal_id.display_name))
+
+        # Compute amounts.
+        write_off_amount_currency = write_off_line_vals.get('amount', 0.0)
+
+        if self.payment_type == 'inbound':
+            # Receive money.
+            liquidity_amount_currency = self.amount
+        elif self.payment_type == 'outbound':
+            # Send money.
+            liquidity_amount_currency = -self.amount
+            write_off_amount_currency *= -1
+        elif self.payment_type == 'transfer':
+            liquidity_amount_currency = self.amount
+        else:
+            liquidity_amount_currency = write_off_amount_currency = 0.0
+
+        write_off_balance = self.currency_id._convert(
+            write_off_amount_currency,
+            self.company_id.currency_id,
+            self.company_id,
+            self.date,
+        )
+        liquidity_balance = self.currency_id._convert(
+            liquidity_amount_currency,
+            self.company_id.currency_id,
+            self.company_id,
+            self.date,
+        )
+        counterpart_amount_currency = -liquidity_amount_currency - write_off_amount_currency
+        counterpart_balance = -liquidity_balance - write_off_balance
+        currency_id = self.currency_id.id
+
+        if self.is_internal_transfer:
+            if self.payment_type == 'inbound':
+                liquidity_line_name = _('Transfer to %s', self.journal_id.name)
+            else: # payment.payment_type == 'outbound':
+                liquidity_line_name = _('Transfer from %s', self.journal_id.name)
+        else:
+            liquidity_line_name = self.payment_reference
+
+        # Compute a default label to set on the journal items.
+
+        payment_display_name = self._prepare_payment_display_name()
+
+        default_line_name = self.env['account.move.line']._get_default_line_name(
+            _("Internal Transfer") if self.is_internal_transfer else payment_display_name['%s-%s' % (self.payment_type, self.partner_type)],
+            self.amount,
+            self.currency_id,
+            self.date,
+            partner=self.partner_id,
+        )
+
+        line_vals_list = [
+            # Liquidity line.
+            {
+                'name': liquidity_line_name or default_line_name,
+                'date_maturity': self.date,
+                'amount_currency': liquidity_amount_currency,
+                'currency_id': currency_id,
+                'debit': liquidity_balance if liquidity_balance > 0.0 else 0.0,
+                'credit': -liquidity_balance if liquidity_balance < 0.0 else 0.0,
+                'partner_id': self.partner_id.id,
+                'account_id': self.journal_id.payment_credit_account_id.id if liquidity_balance < 0.0 else self.journal_id.payment_debit_account_id.id,
+            },
+            # Receivable / Payable.
+            {
+                'name': self.payment_reference or default_line_name,
+                'date_maturity': self.date,
+                'amount_currency': counterpart_amount_currency,
+                'currency_id': currency_id,
+                'debit': counterpart_balance if counterpart_balance > 0.0 else 0.0,
+                'credit': -counterpart_balance if counterpart_balance < 0.0 else 0.0,
+                'partner_id': self.partner_id.id,
+                'account_id': self.destination_account_id.id,
+            },
+        ]
+        if not self.currency_id.is_zero(write_off_amount_currency):
+            # Write-off line.
+            line_vals_list.append({
+                'name': write_off_line_vals.get('name') or default_line_name,
+                'amount_currency': write_off_amount_currency,
+                'currency_id': currency_id,
+                'debit': write_off_balance if write_off_balance > 0.0 else 0.0,
+                'credit': -write_off_balance if write_off_balance < 0.0 else 0.0,
+                'partner_id': self.partner_id.id,
+                'account_id': write_off_line_vals.get('account_id'),
+            })
+        return line_vals_list
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        payments = super().create(vals_list)
+        for i, pay in enumerate(vals_list):
+            payments[i].payment_type = pay.get('payment_type')
+        return payments
