6873e3fddc1474fa9a292ff69530d128fa06b26e
Ignacio Buioli <ibuioli@gmail.com>
2023-09-14 17:28:59 -0300

    fix: qr optimization

--- l10n_ar_report_fe/models.py
+++ l10n_ar_report_fe/models.py
@@ -5,6 +5,7 @@ from odoo.exceptions import ValidationError, RedirectWarning
 import logging
 import datetime
 from datetime import datetime, timedelta, date
+from odoo.tools import float_repr
 import json
 try:
     from base64 import encodebytes
@@ -32,40 +33,44 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def _compute_json_qr(self):
+        # Faster QR generation - based on AdHoc
         for rec in self:
-            dict_invoice = ''
-            if rec.move_type in ['out_invoice','out_refund'] and rec.state == 'posted' and rec.afip_auth_code != '':
-                try:
-                    dict_invoice = {
-                        "ver": 1,
-                        "fecha": str(rec.invoice_date),
-                        "cuit": int(rec.company_id.partner_id.vat),
-                        "ptoVta": rec.journal_id.l10n_ar_afip_pos_number,
-                        "tipoCmp": int(rec.l10n_latam_document_type_id.code),
-                        "nroCmp": int(rec.name.split('-')[2]),
-                        "importe": rec.amount_total,
-                        "moneda": rec.currency_id.l10n_ar_afip_code,
-                        "ctz": rec.l10n_ar_currency_rate,
-                        "tipoDocRec": int(rec.partner_id.l10n_latam_identification_type_id.l10n_ar_afip_code),
-                        "nroDocRec": int(rec.partner_id.vat),
-                        "tipoCodAut": 'E',
-                        "codAut": rec.afip_auth_code,
-                        }
-                except:
-                    dict_invoice = 'ERROR'
-                    pass
-                res = str(dict_invoice).replace("\n", "")
+            if rec.afip_auth_mode in ["CAE", "CAEA"] and rec.afip_auth_code:
+                number_parts = self._l10n_ar_get_document_number_parts(
+                    rec.l10n_latam_document_number, rec.l10n_latam_document_type_id.code
+                )
+
+                qr_dict = {
+                    "ver": 1,
+                    "fecha": str(rec.invoice_date),
+                    "cuit": int(rec.company_id.partner_id.l10n_ar_vat),
+                    "ptoVta": number_parts["point_of_sale"],
+                    "tipoCmp": int(rec.l10n_latam_document_type_id.code),
+                    "nroCmp": number_parts["invoice_number"],
+                    "importe": float(float_repr(rec.amount_total, 2)),
+                    "moneda": rec.currency_id.l10n_ar_afip_code,
+                    "ctz": float(float_repr(rec.l10n_ar_currency_rate, 2)),
+                    "tipoCodAut": "E" if rec.afip_auth_mode == "CAE" else "A",
+                    "codAut": int(rec.afip_auth_code),
+                }
+                if (
+                    len(rec.commercial_partner_id.l10n_latam_identification_type_id)
+                    and rec.commercial_partner_id.vat
+                ):
+                    qr_dict["tipoDocRec"] = int(
+                        rec.commercial_partner_id.l10n_latam_identification_type_id.l10n_ar_afip_code
+                    )
+                    qr_dict["nroDocRec"] = int(
+                        rec.commercial_partner_id.vat.replace("-", "").replace(".", "")
+                    )
+                qr_data = base64.encodestring(
+                    json.dumps(qr_dict, indent=None).encode("ascii")
+                ).decode("ascii")
+                qr_data = str(qr_data).replace("\n", "")
+                rec.json_qr = str(qr_dict)
+                rec.texto_modificado_qr = "https://www.afip.gob.ar/fe/qr/?p=%s" % qr_data
             else:
-                res = 'N/A'
-            rec.json_qr = res
-            if type(dict_invoice) == dict:
-                enc = res.encode()
-                b64 = encodebytes(enc)
-                b64 = encodebytes(json.dumps(dict_invoice, indent=None).encode('ascii')).decode('ascii')
-                b64 = str(b64).replace("\n", "")
-                rec.texto_modificado_qr = 'https://www.afip.gob.ar/fe/qr/?p=' + str(b64)
-            else:
-                rec.texto_modificado_qr = 'https://www.afip.gob.ar/fe/qr/?ERROR'
+                rec.texto_modificado_qr = False
     
     json_qr = fields.Char("JSON QR AFIP",compute=_compute_json_qr)
     texto_modificado_qr = fields.Char('Texto Modificado QR',compute=_compute_json_qr)
