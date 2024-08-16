9d6db904716f46e846f4d7517cc29052e8f964ac
Ignacio Buioli <ibuioli@gmail.com>
2024-08-16 00:59:28 -0300

    feat: ver. arch

--- l10n_ar_bank/__manifest__.py
+++ l10n_ar_bank/__manifest__.py
@@ -1,16 +1,16 @@
 {
     "name": "Listado de Bancos Argentinos",
+    'icon': '/account/static/description/l10n.png',
+    'countries': ['ar'],
     'version': '17.0.1.0.0',
     'category': 'Accounting/Localizations',
-    'sequence': 14,
     'author': 'ADHOC SA, Moldeo Interactive, Exemax, Codize, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'summary': '',
     'depends': [
+        'account',
         'base',
     ],
-    'external_dependencies': {
-    },
     'data': [
         'data/res_bank.xml',
         'views/l10n_ar_bank.xml',
