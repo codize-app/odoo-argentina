054e29282a43feb3b8390c441ade025ba09142b1
Ignacio Buioli <ibuioli@gmail.com>
2024-11-12 18:24:41 -0300

    fix: res.currency ver 17.0

--- account_bimonetary/models/account_move_line.py
+++ account_bimonetary/models/account_move_line.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import logging
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
@@ -9,14 +9,14 @@ class AccountMoveLine(models.Model):
     amount_currency_usd = fields.Monetary(
         string='Monto en USD',
         group_operator=None,
-        compute='_compute_amount_currency_usd', store=True, readonly=False, precompute=True,
+        compute='_compute_amount_currency_usd', readonly=False,
         help="Monto expresado en Dólares USD")
-    currency_usd = fields.Many2one('res.currency', default=2, readonly=True)
+    currency_usd = fields.Many2one(string='Moneda USD', 'res.currency', default=1, readonly=True)
 
     @api.depends('balance')
     def _compute_amount_currency_usd(self):
         for line in self:
             if line.currency_id != line.currency_usd:
-                line.amount_currency_usd = line.currency_usd.round( line.company_id.currency_id.with_context(date=line.date).compute(line.balance, line.currency_usd) )
+                line.amount_currency_usd = line.company_id.currency_id._convert(line.balance, line.currency_usd, line.company_id, line.date)
             else:
                 line.amount_currency_usd = line.amount_currency
