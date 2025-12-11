ef2c3919856b231f9c68851111f39cd69609d0e6
Ignacio Buioli <ibuioli@gmail.com>
2025-12-11 13:41:37 -0300

    fix: add province code missed

--- l10n_ar_report_withholdings_suffered/models/report_withholdings_suffered.py
+++ l10n_ar_report_withholdings_suffered/models/report_withholdings_suffered.py
@@ -338,6 +338,12 @@ class ReportWithholdingsSuffered(models.Model):
             return '923'
         elif self.country_state.code == 'T':#'Perc IIBB Tucuman'
             return '924'
+        elif self.country_state.code == 'G':#'Perc IIBB Santiago del Estero'
+            return '922'
+        elif self.country_state.code == 'H':#'Perc IIBB Chaco'
+            return '906'
+        elif self.country_state.code == 'F':#'Perc IIBB La Rioja'
+            return '912'
         else:
             raise ValidationError('No se encontro un Nº de jurisdiccion para {0}'.format(self.country_state.name))
 
