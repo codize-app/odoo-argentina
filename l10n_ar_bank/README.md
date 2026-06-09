516da6f37ea47cd11cca0b45f2efdbab69307db2
Ignacio Buioli <ibuioli@gmail.com>
2026-06-09 19:20:17 -0300

    feat: add l10n_ar_bank

---
+++ l10n_ar_bank/README.md
@@ -0,0 +1,16 @@
+# Add of Banks CBU and list of Argentine Banks
+
+List of financial entities authorized by the BCRA.
+
+This module allows you to have an updated list of the banking entities of the Argentine Republic. 
+
+## Contributors
+
+* AdHoc SA
+* Moldeo Interactive
+* Exemax
+* Codize
+
+## Maintainer
+
+This module is maintained by the Exemax-Codize team. Original develop by AdHoc SA
---
+++ l10n_ar_bank/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Listado de Bancos Argentinos",
+    'icon': '/account/static/description/l10n.png',
+    'countries': ['ar'],
+    'version': '19.0.1.0.0',
+    'category': 'Accounting/Localizations',
+    'author': 'ADHOC SA, Moldeo Interactive, Exemax, Codize, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'account',
+        'base',
+    ],
+    'data': [
+        'data/res_bank.xml',
+        'views/l10n_ar_bank.xml',
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
