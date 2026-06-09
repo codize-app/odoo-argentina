7c1740116a1e35516b186fd49f6c269046d704b9
Ignacio Buioli <ibuioli@gmail.com>
2026-06-09 15:59:33 -0300

    feat: add account_bimonetary

---
+++ account_bimonetary/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Bimonetary",
+
+    'summary': """
+        Account Bimonetary Reports for Odoo""",
+
+    'description': """
+        Account Bimonetary for Odoo
+    """,
+
+    "icon": '/account/static/description/icon.png',
+
+    'author': "Exemax, Codize",
+    'website': "https://www.exemax.com.ar",
+
+    'category': 'Account',
+    'version': '19.0.0.0.1',
+
+    'depends': ['base', 'account'],
+
+    'data': [
+        'views/account_move_line.xml',
+    ]
+}
