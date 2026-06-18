0508ab951a27d4a69ce1ffa1714bbe570932f784
Ignacio Buioli <ibuioli@gmail.com>
2026-06-17 21:21:02 -0300

    feat: l10n_ar_afipws_fe [test version, not use]

---
+++ l10n_ar_afipws_fe/models/ir_sequence.py
@@ -0,0 +1,28 @@
+from odoo import tools,fields, models, api, _
+from datetime import date
+from odoo.exceptions import ValidationError
+
+class IrSequence(models.Model):
+    _inherit = 'ir.sequence'
+
+    l10n_latam_document_type_id = fields.Many2one('l10n_latam.document.type', 'Tipo de Documento')
+    journal_id = fields.Many2one('account.journal', 'Diario Facturación')
+
+    def comp_consultar(self):
+        self.ensure_one()
+        if not self.journal_id or not self.l10n_latam_document_type_id:
+            raise ValidationError('Faltan datos para consultar AFIP')
+        vals = {
+                'journal_id': self.journal_id.id,
+                'doc_type': self.l10n_latam_document_type_id.id,
+                }
+        wizard_id = self.env['wizard.comp.consultar'].create(vals)
+        return {
+            'name': _('Consultar comprobante'),
+            'res_model': 'wizard.comp.consultar',
+            'view_mode': 'form',
+            'res_id': wizard_id.id,
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+        }
+
