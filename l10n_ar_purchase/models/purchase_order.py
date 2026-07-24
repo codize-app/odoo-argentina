091b68ec9a7d5d409f547c04737586f76f7b4ee0
rojasnelson-dot <rojasnelson@exemax.com.ar>
2026-07-24 00:38:03 -0300

    feat: auto-update supplier price and currency on PO confirm

--- l10n_ar_purchase/models/purchase_order.py
+++ l10n_ar_purchase/models/purchase_order.py
@@ -1,10 +1,15 @@
-from odoo.exceptions import UserError
-from odoo import api, fields, models,Command
-import logging
-_logger=logging.getLogger(__name__)
+# -*- coding: utf-8 -*-
 
-class product_purchase_othercurrency(models.Model):
+from odoo import api, fields, models, Command
+from odoo.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
+
     currency_invoice_id = fields.Many2one('res.currency', string='Currency')
     tipo_cambio_othercurrency = fields.Float(string='Tipo de cambio')
 
@@ -13,19 +18,17 @@ class product_purchase_othercurrency(models.Model):
         for order in self:
             _logger.info('nombre' + order.currency_id.name)
             if order.currency_id.name != 'ARS':
-                    _logger.info('rate'+ str(order.currency_id.rate))
-                    order.tipo_cambio_othercurrency = 1 / order.currency_id.rate
+                _logger.info('rate' + str(order.currency_id.rate))
+                order.tipo_cambio_othercurrency = 1 / order.currency_id.rate
             else:
-                    order.tipo_cambio_othercurrency = 1
+                order.tipo_cambio_othercurrency = 1
 
-    def _set_currency_invoice(self, currency_choice,tipo_cambio_choice):
+    def _set_currency_invoice(self, currency_choice, tipo_cambio_choice):
         for purchase in self:
-            purchase.currency_invoice_id= currency_choice
+            purchase.currency_invoice_id = currency_choice
             purchase.tipo_cambio_othercurrency = tipo_cambio_choice
 
     def _prepare_invoice(self):
-        """Prepare the dict of values to create the new invoice for a purchase order.
-        """
         self.ensure_one()
         _logger.info('=== _prepare_invoice PO: %s ===', self.name)
         _logger.info('currency_id (pedido): %s', self.currency_id.name)
@@ -34,7 +37,9 @@ class product_purchase_othercurrency(models.Model):
         move_type = self._context.get('default_move_type', 'in_invoice')
 
         partner_invoice = self.env['res.partner'].browse(self.partner_id.address_get(['invoice'])['invoice'])
-        partner_bank_id = self.partner_id.commercial_partner_id.bank_ids.filtered_domain(['|', ('company_id', '=', False), ('company_id', '=', self.company_id.id)])[:1]
+        partner_bank_id = self.partner_id.commercial_partner_id.bank_ids.filtered_domain(
+            ['|', ('company_id', '=', False), ('company_id', '=', self.company_id.id)]
+        )[:1]
         currency = self.currency_invoice_id or self.currency_id
 
         invoice_vals = {
@@ -54,26 +59,78 @@ class product_purchase_othercurrency(models.Model):
         }
         return invoice_vals
 
+    def button_confirm(self):
+        res = super().button_confirm()
+        self._exe_update_supplier_prices()
+        return res
+
+    def _exe_update_supplier_prices(self):
+        for order in self:
+            partner = order.partner_id
+            currency = order.currency_id
+
+            _logger.info("EXE_PURCHASE >>> Orden: %s | Proveedor: %s | Moneda: %s (id=%s)",
+                         order.name, partner.name, currency.name, currency.id)
+
+            for line in order.order_line:
+                if line.display_type:
+                    continue
+                product = line.product_id.product_tmpl_id
+                if not product:
+                    continue
+
+                price_unit = line.price_unit
+                if not price_unit:
+                    continue
+
+                _logger.info("EXE_PURCHASE >>> Línea: %s | Precio: %s | Moneda orden: %s",
+                             line.product_id.name, price_unit, currency.name)
+
+                supplierinfo = self.env['product.supplierinfo'].search([
+                    ('partner_id', '=', partner.id),
+                    ('product_tmpl_id', '=', product.id),
+                ], order='id asc', limit=1)
+
+                if supplierinfo:
+                    _logger.info("EXE_PURCHASE >>> Actualizando supplierinfo id=%s | precio=%s | currency_id=%s",
+                                 supplierinfo.id, price_unit, currency.id)
+                    supplierinfo.sudo().write({
+                        'price': price_unit,
+                        'currency_id': currency.id,
+                    })
+                    _logger.info("EXE_PURCHASE >>> Después del write: currency_id=%s",
+                                 supplierinfo.currency_id.name)
+                else:
+                    self.env['product.supplierinfo'].sudo().create({
+                        'partner_id': partner.id,
+                        'product_tmpl_id': product.id,
+                        'product_id': line.product_id.id,
+                        'price': price_unit,
+                        'currency_id': currency.id,
+                        'delay': 1,
+                    })
+
+
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     def _prepare_account_move_line(self, move=False):
         self.ensure_one()
-        aml_currency = move and move.currency_id or self.currency_id
 
         price_unit = self.price_unit
         _logger.info('=== _prepare_account_move_line ===')
         _logger.info('product: %s | price_unit original: %s', self.product_id.name, price_unit)
         _logger.info('currency pedido: %s | currency_invoice: %s',
-            self.order_id.currency_id.name,
-            self.order_id.currency_invoice_id.name if self.order_id.currency_invoice_id else 'VACIO')
-        if (self.order_id.currency_id.name != self.order_id.currency_invoice_id.name):
+                     self.order_id.currency_id.name,
+                     self.order_id.currency_invoice_id.name if self.order_id.currency_invoice_id else 'VACIO')
+
+        if self.order_id.currency_invoice_id and self.order_id.currency_id.name != self.order_id.currency_invoice_id.name:
             price_unit = self.price_unit * self.order_id.tipo_cambio_othercurrency
             _logger.info('Conversión aplicada: %s x %s = %s',
-            self.price_unit, self.order_id.tipo_cambio_othercurrency, price_unit)
+                         self.price_unit, self.order_id.tipo_cambio_othercurrency, price_unit)
         else:
             _logger.info('Sin conversión (misma moneda)')
-        date = move and move.date or fields.Date.today()
+
         res = {
             'display_type': self.display_type or 'product',
             'name': '%s: %s' % (self.order_id.name, self.name),
@@ -81,11 +138,11 @@ class PurchaseOrderLine(models.Model):
             'product_uom_id': self.product_uom_id.id,
             'quantity': self.qty_to_invoice,
             'price_unit': price_unit,
-            #'tax_ids': [(6, 0, self.taxes_id.ids)],
-            'tax_ids': [Command.set(self.tax_ids.ids)],   # ← sintaxis v19
+            'tax_ids': [Command.set(self.tax_ids.ids)],
             'purchase_line_id': self.id,
         }
+
         if self.analytic_distribution and not self.display_type:
             res['analytic_distribution'] = self.analytic_distribution
-        return res
 
+        return res
